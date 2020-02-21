1f12f6137d718dde022633a35c4aa21b7deb440b
jacksonrr3 <49640784+jacksonrr3@users.noreply.github.com>
2020-02-21 12:15:50 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,17 @@ int main(int argc, char* argv[])
 {
     try
     {
-        /*
+        
         if (argc < 2)
         {
             std::cerr << "Usage: chat_server <port>\n";
             return 1;
         }
-        */
+        
+        
         ba::io_service io_service;
-        //ba::ip::tcp::endpoint endpoint(ba::ip::tcp::v4(), std::atoi(argv[1]));
-        ba::ip::tcp::endpoint endpoint(ba::ip::tcp::v4(), 9000);
+        ba::ip::tcp::endpoint endpoint(ba::ip::tcp::v4(), std::atoi(argv[1]));
+       // ba::ip::tcp::endpoint endpoint(ba::ip::tcp::v4(), 9000);
         Server data_server(io_service, endpoint);
         io_service.run();
 
